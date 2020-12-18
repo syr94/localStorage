dbd6592d437820ed1303f35eba34c4795abc6a97
syr94 <55586875+syr94@users.noreply.github.com>
2020-12-18 17:26:19 +0500

    firstLocalStorage_1

--- src/Tail/Tail.js
+++ src/Tail/Tail.js
@@ -4,7 +4,7 @@ import Tail_right from './Tail_right'
 import Tail_left from './Tail_left.js'
 
 export default class Tail extends Component {
-    render() {
+    render() { // component of a tail
         return (
             <div className = "tail">
                 <Tail_left />
